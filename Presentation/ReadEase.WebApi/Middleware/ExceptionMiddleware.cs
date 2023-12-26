98d5792de7e844721cb957b796145a0e07327951
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-12-26 19:24:05 +0300

    Exception Middleware

---
+++ Presentation/ReadEase.WebApi/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using ReadEase.Persistance.Context;
+
+
+namespace ReadEase.WebApi.Middleware;
+
+public class ExceptionMiddleware : IMiddleware
+{
+    private readonly BaseDbContext _context;
+
+    public ExceptionMiddleware(BaseDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            //await LogExceptionToDatabaseAsync(ex, context.Request);
+            //await HandleExceptionAsync(context, ex);
+        }
+    }
+}
